8b9f607de458ce22edad240082ef1ba58f5281d7
Ramadan Timer <app@ramadantimer.com>
2026-01-15 19:58:03 +0300

    Menu pozisyonu asagi kaydirildi - top:35px - v85

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'ramadan-timer-v106';
+const CACHE_NAME = 'ramadan-timer-v107';
 const ASSETS = [
     './',
     './index.html',
-    './style.main.css?v=84',
-    './app.js?v=84',
-    './manifest.json?v=84',
+    './style.main.css?v=85',
+    './app.js?v=85',
+    './manifest.json?v=85',
     './crystal_gem.svg',
     './icon-kabe-192.png',
     './icon-kabe-512.png',
